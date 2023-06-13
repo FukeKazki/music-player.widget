54a73232e6a0e24b2b34a583d07c727182eb33c6
FukeKazki <kazkichi0906@gmail.com>
2023-06-13 23:17:04 +0900

    リファクタリング

--- music-player.widget/music-player.jsx
+++ music-player.widget/music-player.jsx
@@ -6,65 +6,67 @@ export const refreshFrequency = 2000; // ms
 
 export const className = `
   font-family: Source Han Code JP, Helvetica Neue;
-  top: 8px;
-  left: 8px;
+  top: 16px;
+  left: 16px;
 `;
-const containerCss = css`
-  display: grid;
-  padding: 16px;
-  grid-template-columns: 88px 1fr;
-  gap: 16px;
-  width: 280px;
-  color: #fff;
-  border-radius: 4px;
-  background-color: rgba(255, 255, 255, 0.1);
-  backdrop-filter: blur(20px);
-`
-const imgCss = css`
-  width: 88px;
-  height: 88px;
-  border-radius: 4px;
-  object-fit: cover;
-`
 
-const contentsCss = css`
-  display: flex;
-  flex-direction: column;
-`
-
-const artistCss = css`
-  font-weight: bold;
-  margin: 0;
-  font-size: 12px;
-`
-const titleCss = css`
-  margin: 0;
-  margin-top: 4px;
-  font-size: 8px;
-  font-weight: bold;
-`
-const barCss = css`
-  width: 100%;
-  height: 4px;
-  border-radius: 2px;
-  background-color: rgba(255, 255, 255, 0.2);
-  span {
-    display: block;
-    height: 100%;
+const styles = {
+  container: css`
+    display: grid;
+    padding: 16px;
+    grid-template-columns: 88px 1fr;
+    gap: 16px;
+    width: 280px;
+    color: #fff;
+    border-radius: 4px;
+    background-color: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(20px);
+  `,
+  img: css`
+    width: 88px;
+    height: 88px;
+    border-radius: 4px;
+    object-fit: cover;
+  `,
+  contents:  css`
+    display: flex;
+    flex-direction: column;
+  `,
+  artist: css`
+    font-weight: bold;
+    margin: 0;
+    font-size: 12px;
+  `,
+  title: css`
+    margin: 0;
+    margin-top: 4px;
+    font-size: 8px;
+    font-weight: bold;
+  `,
+  bar: css`
+    width: 100%;
+    height: 4px;
     border-radius: 2px;
-    background-color: #fff;
-  }
-`
+    background-color: rgba(255, 255, 255, 0.2);
+    span {
+      display: block;
+      height: 100%;
+      border-radius: 2px;
+      background-color: #fff;
+    }
+  `
+}
+
 
 export const render = ({ output, ...props }) => {
   const [title, artist, artworkData, artworkMIMEType, duration, elapsedTime] = output.split('\n')
   return (
-    <div className={containerCss}>
-      <img className={imgCss} src={`data:${artworkMIMEType};base64,${artworkData}`} width="88" height="88"/>
-      <div className={contentsCss}>
-        <p className={artistCss}>{artist}</p>
-        <p className={titleCss}>{title}</p>
-        <p className={barCss}>
+    <div className={styles.container}>
+      <img className={styles.img} src={`data:${artworkMIMEType};base64,${artworkData}`} width="88" height="88"/>
+      <div className={styles.contents}>
+        <p className={styles.artist}>{artist}</p>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.bar}>
           <span style={{width: `${100*elapsedTime/duration}%`}}></span>
         </p>
       </div>
